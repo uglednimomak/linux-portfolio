cab7a19f504cc6e9b8ef2fa5f55cfcb8b9ec910e
uglednimomak <ante93366@gmail.com>
2025-11-26 15:14:45 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
